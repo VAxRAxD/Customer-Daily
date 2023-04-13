88c1923c19a8ef08044e727d19bf80a83d0841ee
Varad Prabhu <67188972+VAxRAxD@users.noreply.github.com>
2023-04-13 23:29:15 +0530

    Update settings.py

--- CustomerDaily/settings.py
+++ CustomerDaily/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vaxraxd.pythonanywhere.com/']
+ALLOWED_HOSTS = ['vaxraxd.pythonanywhere.com']
 
 
 # Application definition
